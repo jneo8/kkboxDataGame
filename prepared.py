d8140936367f765f5d80b115f1eed75d41b66fb5
jneo8 <james851212@gmail.com>
2017-06-18 17:04:24 +0000

    * prepared add method time_to_month

--- prepared.py
+++ prepared.py
@@ -5,6 +5,7 @@ from datetime import datetime
 
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 from confs import logconf
 
@@ -32,18 +33,31 @@ class Prepared():
         # plt.savefig('events_test')
         # subprocess.call(['catimg', '-f', 'events_test.png'])
 
-    def train(self):
-        """Prepared train data."""
-        for idx, t in enumerate(self.events_train['time'][:1000]):
-            logger.debug('time {}'.format(idx))
+    def time_to_month(self):
+        """Turn time from sec to month."""
+        for idx, t in enumerate(self.events_train['time']):
+            # logger.debug('time {}'.format(idx))
             self.events_train['time'][idx] = datetime.fromtimestamp(t).month
 
-        self.events_train['time'][:1000].hist(bins=50, figsize=(20, 15))
-        plt.savefig('events_train')
-        subprocess.call(['catimg', '-f', 'events_train.png'])
-        self.events_train['title_id'].hist(bins=200, figsize=(20, 15))
-        plt.savefig('events_train-title_id')
-        subprocess.call(['catimg', '-f', 'events_train-title_id.png'])
+        self.events_train.to_csv(
+            os.path.join(DATA_DIR, 'events_train_v1.csv')
+        )
+
+    def train(self):
+        """Prepared train data."""
+        self.events_train['watch_time'].hist(bins=500, figsize=(10, 15))
+        plt.savefig('events_train_watch_time')
+        subprocess.call(['catimg', '-f', 'events_train_watch_time.png'])
+
+        self.events_train['ww'] = np.rint(self.events_train['watch_time'] / 1000)
+        logger.debug(self.events_train['ww'].value_counts())
+        logger.debug(self.events_train.head())
+        # self.events_train.to_csv(
+        #     os.path.join(DATA_DIR, 'events_train123.csv'),
+        # )
+        # self.events_train['title_id'].hist(bins=200, figsize=(20, 15))
+        # plt.savefig('events_train-title_id')
+        # subprocess.call(['catimg', '-f', 'events_train-title_id.png'])
 
 
 if __name__ == '__main__':
