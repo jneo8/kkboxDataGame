94392bf8a3148ba32971d5d86797feb9c44fc940
jneo8 <james851212@gmail.com>
2017-06-20 23:08:01 +0800

    SVM

--- train.py
+++ train.py
@@ -6,6 +6,7 @@ from datetime import datetime
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+from sklearn import svm
 
 from confs import logconf
 
@@ -25,10 +26,25 @@ class Train():
 
     def __init__(self):
         """Init."""
-        train_x = pd.read_csv(os.path.join(DATA_DIR, 'events_train_v1.csv'))
+        # train_x = pd.read_csv(os.path.join(DATA_DIR, 'events_train_v1.csv'))
+        train_x = read(name='events_train_v1')
+        self.train_x = train_x.drop(train_x.columns[[0, 1]], axis=1)
 
-        # info of events_train
-        logger.debug(train_x.head())
+        label_x = read(name='labels_train')
+        self.label_x = label_x['title_id'].values.tolist()
+
+        self.num = 10000
+
+    def main(self):
+        """Step."""
+        self._svm()
+        # logger.debug(self.label_x[:200])
+
+    def _svm(self):
+        """SVM."""
+        clf = svm.SVC()
+        clf.fit(self.train_x, self.label_x)
 
 if __name__ == '__main__':
     t = Train()
+    t.main()
